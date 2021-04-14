0987c5773bad75955f7fd9345b76ece6ed293d2c
taku-Matsunaga <65112145+taku-Matsunaga@users.noreply.github.com>
2021-04-14 15:12:23 +0900

    Delete config.js

--- src/firebase/config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCfA7Mq_MFW-MeWg8T869jGRSV299OyT08",
-  authDomain: "react-firebase-6c3a5.firebaseapp.com",
-  projectId: "react-firebase-6c3a5",
-  storageBucket: "react-firebase-6c3a5.appspot.com",
-  messagingSenderId: "354593220508",
-  appId: "1:354593220508:web:c99e1b40883dc86e6b4a59"
-};
